1129110943e79822a9a1a78e173b8764a5449120
Motoki Naito <motoki.developer@gmail.com>
2019-12-31 22:13:39 +0900

    adding register form

--- src/UserRegisterForm.tsx
+++ src/UserRegisterForm.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 
 const UserRegisterForm: React.SFC = () => {
-  return <div className="user-register-form"></div>;
+  return (
+    <div className="user-register-form">
+      <input type="text" />
+      <input type="text" />
+      <button>Submit</button>
+    </div>
+  );
 };
 
 export default UserRegisterForm;
